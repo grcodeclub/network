df5f759691d02f4a0027f570eef688f07b3bb916
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-08 18:03:23 +0200

    Update menu.js

--- files/menu.js
+++ files/menu.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = '<li><a href="https://grcodeclub.gr/network/post/κατάλογοι">Κατάλογοι</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.gr/network/κατάλογοι">Κατάλογοι</a></li>';
      $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = '<li><a href="https://grcodeclub.gr/network/post/protocol">Πρωτόκολλα</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = '<li><a href="https://grcodeclub.gr/network/post/ορολογία">Ορολογία</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.gr/network/ορολογία">Ορολογία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = '<li><a href="https://grcodeclub.gr/network/post/list">Έγγραφα</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.gr/network/list">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
     menuLink = '<li><a href="https://grcodeclub.gr/cloud/">Cloud</a></li>';
