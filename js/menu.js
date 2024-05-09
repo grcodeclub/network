a41df37ed87fc692d03037d26fa4ea80a118b39e
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-05-09 16:57:34 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = '<li><a href="https://grcodeclub.gr/network/lists">Κατάλογοι</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.gr/network/list">Κατάλογοι</a></li>';
      $('.sidebarMenuInner').append(menuLink);
     
     menuLink = '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>';
