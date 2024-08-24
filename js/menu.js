559227263ebdecc0894c689296e4d81020889271
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-24 22:45:26 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,20 @@
-$(document).ready(function() {
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/more/list/services-port">Υπηρεσίες και Ports</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/more/list/application-and-protocols">Εφαρμογή και Πρωτόκολλο</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/orologia">Ορολογία</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/more">Έγγραφα</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/cloud/">Cloud</a></li>',
+        '<li style="height: 200px;"></li>'
+    ];
     
-    var menuLink = ' <li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = ' <li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.gr/network/more/list/services-port">Υπηρεσίες και Ports</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li><a href="https://grcodeclub.gr/network/more/list/application-and-protocols">Εφαρμογή και Πρωτόκολλο</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li><a href="https://grcodeclub.gr/network/orologia">Ορολογία</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.gr/network/more">Έγγραφα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li><a href="https://grcodeclub.gr/network/cloud/">Cloud</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li style="height: 200px;"></li>';
-    $('.sidebarMenuInner').append(menuLink);
-  });
-  
+    var $sidebarMenuInner = document.querySelector('.sidebarMenuInner');
+    if ($sidebarMenuInner) {
+        menuLinks.forEach(function(link) {
+            $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+        });
+    }
+};
